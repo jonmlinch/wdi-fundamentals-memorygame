2e3a9dd27dd245551cf2d49dc1e55adbe61b4b1c
Jon Linch <jonmlinch@gmail.com>
2018-06-23 19:53:56 -0700

    updated game logic with functions for flips

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,14 +2,18 @@ console.log("up and running");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match")
-	} else { alert("Sorry, try again.")}
+			console.log("You found a match")
+		} else { console.log("Sorry, try again.")}
 }
+var flipCard = function (cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
