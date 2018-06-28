49de360aa6d58d2e9e38119839659d7353e1eb2b
Jon Linch <jonmlinch@gmail.com>
2018-06-27 19:01:21 -0700

    added event listening to allow for card flip

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,18 +26,25 @@ var cardsInPlay = [];
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match")
-		} else { alert("Sorry, try again.")}
-}
-var flipCard = function (cardId) {
+		} else { alert("Sorry, try again.")};
+};
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage)
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
-	}
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	};
+};
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+};
 
-}
-
-flipCard(0);
-flipCard(2);
+createBoard();
